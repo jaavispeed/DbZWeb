8e53437962f740a648bd86a0e9048f692923e4ae
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-05-29 21:50:40 -0400

    se muestra la lista

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { DbzService } from '../../services/dbz.service';
 import { tap } from 'rxjs';
+import { Character } from '../../services/models/characters.interface';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +10,20 @@ import { tap } from 'rxjs';
   templateUrl: './home.component.html',
 })
 export default class HomeComponent implements OnInit {
-
+  //Injects
   private dbzService = inject(DbzService);
-
+  //Variables
+  character: Character[] = [];
 
   ngOnInit(): void {
-
-    this.dbzService.getCharacters().pipe(tap((characters) => {
-      console.log('Characters loaded:', characters);
-    })).subscribe()
+    this.dbzService
+      .getCharacters()
+      .pipe(
+        tap((characters) => {
+          console.log('Characters loaded:', characters);
+          this.character = characters
+        })
+      )
+      .subscribe();
   }
-
 }
--- src/app/services/dbz.service.ts
+++ src/app/services/dbz.service.ts
@@ -1,17 +1,19 @@
 import { inject, Injectable } from '@angular/core';
-import { Character } from './models/characters.interface';
+import { CharacterResponse } from './models/characters.interface';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 const apiCharacters = 'https://dragonball-api.com/api/characters';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DbzService {
   private http = inject(HttpClient);
 
-
-getCharacters() {
-  return this.http.get<Character[]>(apiCharacters);
-}
+  getCharacters() {
+    return this.http.get<CharacterResponse>(apiCharacters).pipe(
+      map((response) => response.items)
+    );
+  }
 }
